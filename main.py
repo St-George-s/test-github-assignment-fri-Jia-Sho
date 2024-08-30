011fc0d6ab777a3fa885179f95044c518996523b
Jia-Sho <sho110@stge.org.uk>
2024-08-30 09:09:38 +0000

    almost compleated task 2

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-for x in range(1000000):
-    print(str(x) + " x " + str(13.5) + " = " + str(x * 13.5))
+def get_destination():
+    destination = input("enter the travel destination ")
+    return destination
+
+def get_number_of_people():
+    people_number = input("Enter the number of people going ")
+    return people_number
+
+def get_travel_method():
+    travel_method = input("Enter your mode of transport ")
+    return travel_method
+
+def print_travel_details(destination, people_count, travel_method):
+    print("Your are going to", destination,"with", people_count,"people by",travel_method)
+
+#main
+
+destination = get_destination()
+people_count = get_number_of_people()
+if destination == "edinburgh" or "glasgow" or "dundee" or "aberdeen":
+    travel_method = "local transport"
+else:
+    travel_method = get_travel_method()
+print_travel_details(destination,people_count,travel_method)
