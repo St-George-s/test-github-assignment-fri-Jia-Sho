69d7eb5facaa81d9de855973d0ce3d26a35f511a
Jia-Sho <sho110@stge.org.uk>
2024-09-27 08:18:24 +0000

    courcework 2023 compleated

---
+++ Coursework/mock courcework.py
@@ -0,0 +1,57 @@
+import csv
+attractions = []
+category = []
+visitors = []
+daysOpen = []
+height = []
+# reading data from csv file to parallel arrays
+def attractions_data():
+    with open("Coursework/attractions.csv", "r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            attractions.append(row[0])
+            category.append(row[1])
+            visitors.append(int(row[2]))
+            daysOpen.append(int(row[3]))
+            height.append(row[4])
+    return attractions, category, visitors, daysOpen , height
+
+
+# finding and display least visited attraction
+def least_visited_attraction(attraction , visitor):
+    minimum = visitor[0]
+    minIndex = 0
+    for i in range(1, len(visitor)):
+        if minimum > visitor[i]:
+            minimum = visitor[i]
+            minIndex = i
+    print("The least visited attraction is", attraction[minIndex], "with", minimum,"visitors.")
+    pass
+
+#finding and display most visited attraction
+def most_visited_attraction(attraction, visitor):
+    maximum = visitor[0]
+    maxIndex = 0
+    for i in range(1, len(visitor)):
+        if maximum < visitor[i]:
+            maximum = visitor[i]
+            maxIndex = i
+    print("The most visited attraction is", attraction[maxIndex],"With", maximum,"visitors.")
+
+
+# find names of roller costers that need service in 7 days and write to text file
+def service_roller_coster(attraction, category, daysOpen):
+    with open("service.csv","w") as file:
+        for i in range (0,len(attraction)):
+            if category[i] == "Roller Coaster":
+                days = daysOpen[i] % 90
+                if 90-days <= 7:
+                    print(attraction[i])
+                    file.write(attraction[i] )
+                    
+
+#main program
+attractions_data()
+least_visited_attraction(attractions, visitors)
+most_visited_attraction(attractions, visitors)
+service_roller_coster(attractions, category, daysOpen)
--- Standard Algorithm/standard algorithms task 3.py
+++ Standard Algorithm/standard algorithms task 3.py
@@ -3,61 +3,63 @@ times_spent = [5, 3, 4, 6, 5]
 student_name = ["Alice", "Ben", "Cara", "David", "Eva"]
 activities = ["hillwalking", "canoeing", "climbing", "hillwalking", "canoeing"]
 
-
+# defining the record
 class Activities:
-    def __init__(self, time)
+    def __init__(self, time, name, activity):
+        self.time = time
+        self.name = name
+        self.activity = activity
+
+# storing in form of arrays of records
+def record_storing(time , name , activity):
+    for i in range(0, len(time)):
+        student_activities = [ Activities(time[i], name[i], activity[i]) ]
+    return student_activities
 
 
-
-
-
-
-
-
-# Find Maximum in parallel arrays
-def find_max(time, name, activity):
-    maximum = time[0]
+# Find Maximum in arrays of records
+def find_max(record):
+    maximum = record[0].time
     maxIndex = 0
-    for i in range(1, len(time)):
-        if maximum < time[i]:
-            maximum = time[i]
+    for i in range(1, len(record)):
+        if maximum < record[i].time:
+            maximum = record[i].time
             maxIndex = i
-    print("The maximum time spent was", maximum ,"hours by", name[maxIndex], "during", activity[maxIndex])
+    print("The maximum time spent was", maximum ,"hours by", record[maxIndex].name, "during", record[maxIndex].activity)
 
-#Find Minimum in parallel arrays
-def find_min(time, name, activity):
-    minimum = time[0]
+#Find Minimum in arrays of records
+def find_min(record):
+    minimum = record[0].time
     maxIndex = 0
-    for i in range(1,len(time)):
-        if minimum > time[i]:
-            minimum = time[i]
+    for i in range(1,len(record)):
+        if minimum < record[i].time :
+            minimum = record[i].time
             maxIndex = i
-    print("The minimum time spent was", minimum ,"hours by", name[maxIndex], "during", activity[maxIndex])
+    print("The minimum time spent was", minimum ,"hours by", record[maxIndex].name, "during", record[maxIndex].activity )
 
-#Linear Search in parallel arrays
-def linear_search(time, name , activity):
+#Linear Search in arrays of records
+def linear_search(record):
     item_searching = int(input("Enter the value to find "))
     index = 0
-    for i in range(0, len(time)):
-        if item_searching == time[i]:
+    for i in range(0, len(record)):
+        if item_searching == record[i].time:
             index = i
-            print("Time", item_searching,"hours was spent by", name[index],"during", activity[index]  )
+            print("Time", item_searching,"hours was spent by", record[index].name ,"during", record[index].activity  )
     
-#count occourances in parallel arrays
-def count_occourances(time, name , activity):
+#count occourances in arrays of records
+def count_occourances(record):
     item_counting = int(input("Enter the item to count "))
     count = 0
     name_count = []
-    for i in range(0, len(time)):
-        if item_counting == time[i]:
+    for i in range(0, len(record)):
+        if item_counting == record[i].time :
             count = count +1
-            name_count.append(name[i])
+            name_count.append(record[i].name )
     print("Time", item_counting,"hours appeared", count,"times. Students", name_count)
 
 # main program
-max_time = find_max(times_spent, student_name, activities)
-
-
-find_min(times_spent, student_name, activities)
-linear_search(times_spent, student_name, activities)
-count_occourances(times_spent, student_name, activities )
+record = record_storing(times_spent, student_name, activities)
+find_max(record)
+find_min(record)
+#linear_search(record)
+#count_occourances(record)
