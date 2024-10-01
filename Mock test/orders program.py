28dc9d1d0c3c617d4ea27e02337955f57000bc51
Jia-Sho <sho110@stge.org.uk>
2024-10-01 09:24:23 +0000

    on part c

--- Mock test/orders program.py
+++ Mock test/orders program.py
@@ -1,10 +1,12 @@
 import csv
 # defining class order
 class Order():
-    def __init__(self, customer_name, product_purchased, amount_spent):
+    def __init__(self, customer_name, product_purchased, amount_spent, customerID , category_product):
         self.customer_name = customer_name
         self.product_purchased = product_purchased
         self.amount_spent = amount_spent
+        self.customerID = customerID
+        self.category_product = category_product
 
 #read csv file in arrays of record
 def readOrdersFromCSV():
@@ -26,6 +28,14 @@ def findMaxOrderWithTv(orders):
                 maxOrder = orders[i].amount_spent
                 maxIndex = i
     print("Maximum spent on TV is", maxOrder,"by", orders[maxIndex].customer_name)
+
+def discount(orders):
+    with open("discount.txt","a") as file:
+    for i in range(0, len(orders)):
+        if orders[i].customerID % 5 == 0:
+            product = orders[i].category_product[:3]
+            file.write(orders[i].customerID, product, "Discount code assigned")
+            
 
 # main program
 orders = readOrdersFromCSV()
