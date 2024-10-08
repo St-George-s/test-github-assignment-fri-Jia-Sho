89077aa861ad0171ce7e34e3e26aed069500cdef
Jia-Sho <sho110@stge.org.uk>
2024-10-08 08:57:20 +0000

    completed class test part b

--- class test/class test part b.py
+++ class test/class test part b.py
@@ -3,21 +3,23 @@ gameTitles= []
 genres = []
 ageRatings = []
 
+#reads the data into parallel arrays
 def readGameDataFromCSV():
     with open("class test/game_data.csv", "r") as file:
         reader = csv.reader(file)
         header = next(reader)
+        #loops over each row and append to corresponding arrays
         for row in reader:
             gameTitles.append(row[0])
             genres.append(row[1])
             ageRatings.append(int(row[2]))
     return gameTitles, genres, ageRatings
 
-
+# finds how many games are in a genre that are under 18
 def countSuitableGames(genre_to_check, gameTitles, genres, ageRatings):
     count = 0
     for i in range(0, len(gameTitles)):
-        if genres[i] == genre_to_check :
+        if genres[i] == genre_to_check : 
             if ageRatings[i] < 18:
                 print(gameTitles[i])
                 count = count +1
