ef825401e8650a4a366897d7d3cfa4350653a974
Jia-Sho <sho110@stge.org.uk>
2024-11-14 14:21:12 +0000

    completed till worksheet 4

--- Spotify/main.sql
+++ Spotify/main.sql
@@ -1,20 +1,12 @@
-SELECT artist_name 
-FROM Artists 
-WHERE artist_name LIKE 'T%';
+-- SELECT COUNT(*)
+-- FROM Tracks
+-- WHERE duration_ms>266000;
+
+SELECT genre_id, COUNT(*)
+FROM Tracks 
+GROUP BY genre_id;
 
 
-SELECT album_name, release_year 
-FROM Albums 
-WHERE release_year >= 2015 
-ORDER BY release_year DESC;
-
-
-SELECT a.album_name AS Album, a.release_year Year 
-FROM Albums a 
-WHERE a.release_year > 2010 
-ORDER BY a.release_year;
-
-
-SELECT artist_name, LENGTH(artist_name) AS Name_Length 
-FROM Artists 
-WHERE artist_name LIKE '_a%';
+-- SELECT genre_id, AVG(duration_ms)
+-- FROM Tracks 
+-- GROUP BY genre_id;
