f51541f7f42a97b7613249d44613c94482ca275d
Jia-Sho <sho110@stge.org.uk>
2024-11-26 09:59:11 +0000

    completed 2023 courcework

--- Courseworks Databases/SQA-22-Coursework-Database-Routes/main.sql
+++ Courseworks Databases/SQA-22-Coursework-Database-Routes/main.sql
@@ -1,1 +1,5 @@
-DESCRIBE ALL TABLES;
+SELECT p.forename, p.surname, p.plannerNo , count(*) AS [Total Participants]
+FROM Planner P, Route R, Walk W, Walker WR
+WHERE p.plannerNo = r.plannerNo
+AND r.routeID = W.routeID
+AND w.walkerno = WR.walkerno;
--- Photography/main.sql
+++ Photography/main.sql
@@ -1,1 +1,7 @@
-DESCRIBE ALL TABLES;
+SELECT COUNT(*) AS [Photo Count] 
+FROM Photos 
+WHERE photographer_id IN ( 
+  SELECT photographer_id
+  FROM Photographers 
+  WHERE experience_years > 5 
+); 
