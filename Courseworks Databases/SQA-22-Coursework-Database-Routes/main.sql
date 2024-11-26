e1fc62791fc4fdf393aeb02d8f50b0a4b70b2252
Jia-Sho <sho110@stge.org.uk>
2024-11-26 09:59:11 +0000

    completed 2023 courcework

--- Courseworks Databases/SQA-22-Coursework-Database-Routes/main.sql
+++ Courseworks Databases/SQA-22-Coursework-Database-Routes/main.sql
@@ -1,5 +1,1 @@
-SELECT p.forename, p.surname, p.plannerNo , count(*) AS [Total Participants]
-FROM Planner P, Route R, Walk W, Walker WR
-WHERE p.plannerNo = r.plannerNo
-AND r.routeID = W.routeID
-AND w.walkerno = WR.walkerno;
+DESCRIBE ALL TABLES;
