12f81bcd50a8410bb100cfd4ca1a5b8dce01b8ef
Jia-Sho <sho110@stge.org.uk>
2025-02-25 10:23:24 +0000

    databases

--- Courswork 2025/coursework-2025/main.sql
+++ Courswork 2025/coursework-2025/main.sql
@@ -1,1 +1,40 @@
-DESCRIBE ALL TABLES;
+
+
+-- SELECT Comic.comicTitle, Comic.issue, Publisher.publisherName, Comic.valuation
+-- FROM Comic, Publisher
+-- WHERE Comic.publisherID = Publisher.publisherID
+-- AND Comic.valuation  IN (
+--     SELECT AVG(valuation)
+--     FROM Comic;
+-- );
+
+-- SELECT AVG(valuation)
+-- FROM Comic;
+
+
+
+
+-- SELECT CharacterInfo.characterName, SUM(Comic.valuation) AS [Total Valuation]
+-- FROM CharacterInfo, Comic, ComicCharacter
+-- WHERE ComicCharacter.characterID = CharacterInfo.characterID
+-- AND ComicCharacter.comicID = Comic.comicID
+-- AND CharacterInfo.characterName LIKE '%Duck%'
+-- GROUP BY CharacterInfo.characterName;
+
+
+
+-- ComicCharacter.mainCharacter
+
+
+
+
+SELECT CharacterInfo.characterName, SUM(Comic.valuation) AS [Total Valuation]
+FROM CharacterInfo, Comic, ComicCharacter
+WHERE ComicCharacter.mainCharacter LIKE '%Duck%'
+AND ComicCharacter.characterID = CharacterInfo.characterID
+AND ComicCharacter.comicID = Comic.comicID
+GROUP BY CharacterInfo.characterName;
+
+
+
+
