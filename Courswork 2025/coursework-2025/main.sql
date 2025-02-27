f2f2fa28e9b17f2ec55ae0fa3ac360e564e03f1a
Jia-Sho <sho110@stge.org.uk>
2025-02-27 14:26:02 +0000

    database coursework

--- Courswork 2025/coursework-2025/main.sql
+++ Courswork 2025/coursework-2025/main.sql
@@ -1,40 +1,32 @@
 
-
+-- compleated and working 2c
 -- SELECT Comic.comicTitle, Comic.issue, Publisher.publisherName, Comic.valuation
 -- FROM Comic, Publisher
 -- WHERE Comic.publisherID = Publisher.publisherID
--- AND Comic.valuation  IN (
---     SELECT AVG(valuation)
---     FROM Comic;
--- );
-
--- SELECT AVG(valuation)
--- FROM Comic;
+-- AND Comic.valuation  > 300 + (SELECT AVG(valuation) FROM Comic);
 
 
 
-
+-- COMPLEATED & WORKING 2D
 -- SELECT CharacterInfo.characterName, SUM(Comic.valuation) AS [Total Valuation]
 -- FROM CharacterInfo, Comic, ComicCharacter
 -- WHERE ComicCharacter.characterID = CharacterInfo.characterID
 -- AND ComicCharacter.comicID = Comic.comicID
 -- AND CharacterInfo.characterName LIKE '%Duck%'
--- GROUP BY CharacterInfo.characterName;
+-- AND ComicCharacter.mainCharacter = 1
+-- GROUP BY CharacterInfo.characterName
+-- ORDER BY [Total Valuation] DESC;
 
 
 
--- ComicCharacter.mainCharacter
-
-
-
-
-SELECT CharacterInfo.characterName, SUM(Comic.valuation) AS [Total Valuation]
-FROM CharacterInfo, Comic, ComicCharacter
-WHERE ComicCharacter.mainCharacter LIKE '%Duck%'
-AND ComicCharacter.characterID = CharacterInfo.characterID
-AND ComicCharacter.comicID = Comic.comicID
-GROUP BY CharacterInfo.characterName;
-
-
+-- compleated and working 2e
+-- SELECT Comic.comicTitle, Comic.issue, Publisher.publisherName, (Comic.valuation * 2) AS [Double Price]
+-- FROM Comic, Publisher, Series, CharacterInfo, ComicCharacter
+-- WHERE Series.seriesName = "The OK Seven"
+-- AND CharacterInfo.characterName = "Starlordly"
+-- AND Comic.publisherID = Publisher.publisherID
+-- AND Comic.seriesID = Series.seriesID
+-- AND CharacterInfo.characterID = ComicCharacter.characterID
+-- AND Comic.comicID = ComicCharacter.comicID;
 
 
